15955e671fd1a7ad7d7c4b4944eab98ab4232365
Robert Cronin <robert.t.cronin@outlook.com>
2022-05-22 19:06:01 -0700

    added time class

---
+++ src/Time.cpp
@@ -0,0 +1,15 @@
+#include "Time.hpp"
+
+#include <chrono>
+
+Time* Time::s_Instance = new Time();
+
+long Time::GetTimeImpl()
+{
+    return std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch().count() - m_StartTime;
+}
+
+long Time::GetEpochTimeImpl()
+{
+    return std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch().count();
+}
---
+++ src/Time.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <sys/types.h>
+#include <chrono>
+
+class Time
+{
+public:
+    static inline long GetTime() { return s_Instance->GetTimeImpl(); }
+
+    static inline long GetEpochTime() { return s_Instance->GetEpochTimeImpl(); }
+
+protected:
+    virtual long GetTimeImpl();
+    virtual long GetEpochTimeImpl();
+
+private:
+    Time() {}
+    Time(Time& time) {}
+
+    u_int64_t m_StartTime = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch().count();
+
+    static Time* s_Instance;
+};
