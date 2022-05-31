a50748c0a1ff87eb934587cc91d2183de6a47bfb
Robert Cronin <robert.t.cronin@outlook.com>
2022-05-30 20:59:20 -0700

    removed unnessesary header files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,5 @@
 #include "MainLoop.hpp"
 
-#include <glm/ext/matrix_clip_space.hpp>
-#include <glm/fwd.hpp>
-#include <glm/glm.hpp>
-#include <glm/gtx/transform.hpp>
-#include <iostream>
-
 int main()
 {
     MainLoop::run();
